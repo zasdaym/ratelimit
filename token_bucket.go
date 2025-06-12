7342947f219adfda62944b8e47802d93faa9e174
Zasda Yusuf Mikail <zasdaym@gmail.com>
2025-06-12 09:11:44 +0700

    Fix token bucket logic

--- token_bucket.go
+++ token_bucket.go
@@ -3,11 +3,11 @@ package ratelimit
 import "time"
 
 type TokenBucket struct {
-	capacity    int
-	fillRate    int
-	tokens      int
-	lastRequest time.Time
-	clock       Clock
+	capacity  int
+	fillRate  int
+	tokens    int
+	lastAllow time.Time
+	clock     Clock
 }
 
 func NewTokenBucket(capacity, fillRate int, clock Clock) *TokenBucket {
@@ -16,22 +16,22 @@ func NewTokenBucket(capacity, fillRate int, clock Clock) *TokenBucket {
 	}
 	now := clock.Now()
 	return &TokenBucket{
-		capacity:    capacity,
-		fillRate:    fillRate,
-		tokens:      capacity,
-		lastRequest: now,
-		clock:       clock,
+		capacity:  capacity,
+		fillRate:  fillRate,
+		tokens:    capacity,
+		lastAllow: now,
+		clock:     clock,
 	}
 }
 
 func (tb *TokenBucket) Allow() bool {
 	now := tb.clock.Now()
-	elapsed := now.Sub(tb.lastRequest).Seconds()
+	elapsed := now.Sub(tb.lastAllow).Seconds()
 	tb.tokens = min(tb.capacity, tb.tokens+tb.fillRate*int(elapsed))
-	tb.lastRequest = now
 
 	if tb.tokens > 0 {
 		tb.tokens--
+		tb.lastAllow = now
 		return true
 	}
 	return false
